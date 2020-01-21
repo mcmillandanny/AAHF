0b2146f48d13f0b04263142c9f0a54654ea9848b
Dnny <mcmillandanny@gmail.com>
2020-01-21 16:27:14 -0500

    built out nave for mobile

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,32 @@
 "use strict";
 
 console.log("Hello world");
+var hamburgerBtn = document.querySelector(".hamburgerBtn");
+var overlayMobile = document.querySelector(".overlayNavMobile");
+var closeNavBtn = document.querySelector(".closeBtn");
+hamburgerBtn.addEventListener("click", function () {
+  TweenMax.to(overlayMobile, {
+    display: "block",
+    left: "0px",
+    duration: .3,
+    ease: "ease-in"
+  });
+}); // var obj = {prop: 10};
+// gsap.to(obj, {
+//   duration: 1,
+//   prop: 200, 
+//   //onUpdate fires each time the tween updates; we'll explain callbacks later.
+//   onUpdate: function() {
+//     console.log(obj.prop); //logs the value on each update.
+//   }
+// });
+
+closeNavBtn.addEventListener("click", function () {
+  TweenMax.to(overlayMobile, {
+    display: "none",
+    left: "-500px",
+    duration: .3,
+    ease: "ease-in"
+  });
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
 console.log(`Hello world`)
+
+
+let hamburgerBtn = document.querySelector(".hamburgerBtn");
+let overlayMobile = document.querySelector(".overlayNavMobile")
+let closeNavBtn = document.querySelector(".closeBtn");
+
+hamburgerBtn.addEventListener("click", () => {
+    TweenMax.to(overlayMobile, {
+        display: "block",
+        left: "0px",
+        duration: .3,
+        ease: "ease-in"
+    })
+})
+// var obj = {prop: 10};
+// gsap.to(obj, {
+//   duration: 1,
+//   prop: 200, 
+//   //onUpdate fires each time the tween updates; we'll explain callbacks later.
+//   onUpdate: function() {
+//     console.log(obj.prop); //logs the value on each update.
+//   }
+// });
+
+closeNavBtn.addEventListener("click", () => {
+    TweenMax.to(overlayMobile, {
+        display: "none",
+        left: "-500px",
+        duration: .3,
+        ease: "ease-in"
+    })
+})
